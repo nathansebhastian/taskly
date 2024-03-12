d5f406826a7b4f7dc968460a5403cdfe04ffd742
Nathan Sebhastian <nathansebhastian@gmail.com>
2024-03-12 22:52:19 +0700

    Chapter 5: Creating User API Routes and Controller

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,59 @@
+import { db } from '../libs/dbConnect.js';
+import { ObjectId } from 'mongodb';
+import bcrypt from 'bcrypt';
+
+const collection = db.collection('users');
+
+export const test = async (req, res) => {
+  let results = await collection.find({}).toArray();
+  res.status(200).json(results);
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    const query = { _id: new ObjectId(req.params.id) };
+    const user = await collection.findOne(query);
+
+    if (!user) {
+      return next({ status: 404, message: 'User not found!' });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    next({ status: 500, error });
+  }
+};
+
+export const updateUser = async (req, res, next) => {
+  try {
+    if (req.body.password) {
+      req.body.password = await bcrypt.hash(req.body.password, 10);
+    }
+    const query = { _id: new ObjectId(req.params.id) };
+    const data = {
+      $set: {
+        ...req.body,
+        updatedAt: new Date().toISOString(),
+      },
+    };
+    const options = {
+      returnDocument: 'after',
+    };
+
+    const updatedUser = await collection.findOneAndUpdate(query, data, options);
+    const { password: pass, updatedAt, createdAt, ...rest } = updatedUser;
+    res.status(200).json(rest);
+  } catch (error) {
+    next({ status: 500, error });
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  try {
+    const query = { _id: new ObjectId(req.params.id) };
+    await collection.deleteOne(query);
+    res.status(200).json({ message: 'User has been deleted!' });
+  } catch (error) {
+    next({ status: 500, error });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -2,18 +2,19 @@ import express from 'express';
 import 'dotenv/config';
 import { db } from './libs/dbConnect.js';
 
+import userRouter from './routes/user.route.js';
+
 const PORT = 8000;
 const app = express();
 
-app.use('/api', (req, res) => {
-  res.status(200).json({ message: 'Hello World!' });
-});
+app.use(express.json());
+
+app.use('/api/v1/users', userRouter);
 
 app.use('*', (req, res) => {
   res.status(404).json({ message: 'not found' });
 });
 
-// Start the Express server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
