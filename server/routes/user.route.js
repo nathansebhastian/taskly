b5a9a31ee53a6734b760404215d9558033c8a9aa
Nathan Sebhastian <nathansebhastian@gmail.com>
2024-03-13 07:39:42 +0700

    Chapter 6: Creating Error Handler in Express

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,7 +1,6 @@
 import express from 'express';
 
 import {
-  test,
   getUser,
   updateUser,
   deleteUser,
@@ -9,7 +8,6 @@ import {
 
 const router = express.Router();
 
-router.get('/', test);
 router.get('/:id', getUser);
 router.patch('/update/:id', updateUser);
 router.delete('/delete/:id', deleteUser);
--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import 'dotenv/config';
-import { db } from './libs/dbConnect.js';
 
 import userRouter from './routes/user.route.js';
+import { errorHandler } from './libs/middleware.js';
 
 const PORT = 8000;
 const app = express();
@@ -11,13 +11,11 @@ app.use(express.json());
 
 app.use('/api/v1/users', userRouter);
 
-app.use('/', (req, res) => {
-  res.status(200).json({ message: 'Hello World!' });
-});
-
 app.use('*', (req, res) => {
   res.status(404).json({ message: 'not found' });
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
