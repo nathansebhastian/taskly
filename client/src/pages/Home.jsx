ae48f96435d4171c786740af5cc4595313aa3667
Nathan Sebhastian <nathansebhastian@gmail.com>
2024-03-15 15:13:49 +0700

    Chapter 12: Implement Sign In and Sign Out Pages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,58 @@
+import { Link as RouterLink } from 'react-router-dom';
+import productiveSvg from '/productive.svg';
 import {
   Box,
   Heading,
-  Button
+  Text,
+  Link,
+  Flex,
+  Stack,
+  Image,
 } from '@chakra-ui/react';
-
 export default function Home() {
   return (
-    <Box p='4'>
-      <Heading>Home</Heading>
-      <Button colorScheme='teal'>Click Me</Button>
-    </Box>
+    <Flex
+      direction={{ base: 'column', md: 'row' }}
+      gap={6}
+      p={14}
+      maxW='6-xl'
+      mx='auto'
+    >
+      <Stack flex='1' alignSelf='center'>
+        <Heading
+          as='h1'
+          fontSize={{ base: '4xl', lg: '6xl' }}
+          fontWeight='bold'
+          color='gray.700'
+        >
+          Make your{' '}
+          <Text as='span' color='gray.500'>
+            perfect
+          </Text>
+          <br />
+          day
+        </Heading>
+        <Box color='gray.500' pt='8'>
+          Taskly will help you manage your day and to-do list.
+          <br />
+          We have a wide range of features to help you get things done.
+        </Box>
+        <Link
+          as={RouterLink}
+          to={'/profile'}
+          fontWeight='bold'
+          color='blue.400'
+        >
+          Let's get started...
+        </Link>
+      </Stack>
+      <Box flex='1'>
+        <Image
+          src={productiveSvg}
+          maxWidth='400px'
+          alt='Productive Illustration'
+        />
+      </Box>
+    </Flex>
   );
 }
--- client/src/util.js
+++ client/src/util.js
@@ -1,1 +1,21 @@
+import { useState, useEffect } from "react";
+
+export const useLocalStorage = (key, defaultValue) => {
+    const [value, setValue] = useState(() => {
+      const currentValue = localStorage.getItem(key);
+  
+      if (currentValue) {
+        return JSON.parse(currentValue);
+      } else {
+        return defaultValue;
+      }
+    });
+  
+    useEffect(() => {
+      localStorage.setItem(key, JSON.stringify(value));
+    }, [value]);
+  
+    return [value, setValue];
+  };
+
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
