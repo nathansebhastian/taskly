22f05b5f174bc1d977df00800f9ba63adcac2722
Nathan Sebhastian <nathansebhastian@gmail.com>
2024-03-21 10:54:19 +0700

    Chapter 17: Tasks Pagination, Filtering, and Sorting

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -6,10 +6,23 @@ const collection = db.collection('tasks');
 export const getTasksByUser = async (req, res, next) => {
   try {
     const query = { owner: new ObjectId(req.params.id) };
+    const { status, orderBy } = req.query;
+    const sort = orderBy ? { [orderBy]: 1 } : {};
+    if (status) {
+      query['status'] = status;
+    }
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = 4;
 
-    const tasks = await collection.find(query).toArray();
+    const tasks = await collection
+      .find(query)
+      .sort(sort)
+      .limit(pageSize)
+      .skip((page - 1) * pageSize)
+      .toArray();
 
-    res.status(200).json({ tasks });
+    const taskCount = await collection.count(query);
+    res.status(200).json({ tasks, taskCount });
   } catch (error) {
     next({ status: 500, error });
   }
